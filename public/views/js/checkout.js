1bbc51117adeec40f90a5e48623616ee49e27051
Ismaiil23 <mkisma84@gmail.com>
2023-04-17 16:11:04 +0200

    affichage prix total checkout.js

--- public/views/js/checkout.js
+++ public/views/js/checkout.js
@@ -1,22 +1,33 @@
 function displayCartItems() {
-    const cartSummary = document.querySelector('.cart-summary');
-    const cartItems = cart.getItems();
-  
-    cartItems.forEach((item) => {
-      const cartItemDiv = document.createElement('div');
-      cartItemDiv.classList.add('cart-item');
-      cartItemDiv.innerHTML = `
-        <h3>${item.product.name}</h3>
-        <p>Quantité : ${item.quantity}</p>
-        <p>Prix unitaire : ${item.product.price}€</p>
-        <p>Prix total : ${(item.product.price * item.quantity).toFixed(2)}€</p>
-      `;
-      cartItemDiv.addEventListener('click', () => {
-        window.location.href = `/productDetails?productId=${item.product.id}`;
-      });
-      cartSummary.appendChild(cartItemDiv);
+  const cartSummary = document.querySelector('.cart-summary');
+  const cartItems = cart.getItems();
+
+  let totalCost = 0;
+
+  cartItems.forEach((item) => {
+    const cartItemDiv = document.createElement('div');
+    cartItemDiv.classList.add('cart-item');
+    cartItemDiv.innerHTML = `
+      <h3>${item.product.name}</h3>
+      <p>Quantité : ${item.quantity}</p>
+      <p>Prix unitaire : ${item.product.price}€</p>
+      <p>Prix total : ${(item.product.price * item.quantity).toFixed(2)}€</p>
+    `;
+    cartItemDiv.addEventListener('click', () => {
+      window.location.href = `/productDetails?productId=${item.product.id}`;
     });
-  }
+    cartSummary.appendChild(cartItemDiv);
+
+    // update coût total
+    totalCost += item.product.price * item.quantity;
+  });
+
+  // Créer un élément pour afficher le coût total
+  const totalCostElement = document.createElement('h4');
+  totalCostElement.innerHTML = `Coût total : ${totalCost.toFixed(2)}€`;
+  cartSummary.appendChild(totalCostElement);
+}
+
   
   async function isLoggedIn() {
     const response = await fetch('/api/isLoggedIn');
