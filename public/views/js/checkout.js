2999d1948f771a23eed6664985829b9d46cd6516
Ismaiil23 <mkisma84@gmail.com>
2023-04-18 11:45:05 +0200

    modifier quantité produit depuis panier

--- public/views/js/checkout.js
+++ public/views/js/checkout.js
@@ -4,29 +4,70 @@ function displayCartItems() {
 
   let totalCost = 0;
 
+  // élément pour afficher le coût total
+  const totalCostElement = document.createElement('h4');
+  
+  function updateTotalCost() {
+    totalCost = cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
+    totalCostElement.innerHTML = `Coût total : ${totalCost.toFixed(2)}€`;
+  }
+  
   cartItems.forEach((item) => {
     const cartItemDiv = document.createElement('div');
     cartItemDiv.classList.add('cart-item');
     cartItemDiv.innerHTML = `
       <h3>${item.product.name}</h3>
-      <p>Quantité : ${item.quantity}</p>
+      <p>Quantité : <span class="item-quantity">${item.quantity}</span></p>
+      <div>
+        <button type="button" class="decrement">-</button>
+        <button type="button" class="increment">+</button>
+      </div>
       <p>Prix unitaire : ${item.product.price}€</p>
-      <p>Prix total : ${(item.product.price * item.quantity).toFixed(2)}€</p>
+      <p>Prix total : <span class="item-total-price">${(item.product.price * item.quantity).toFixed(2)}</span>€</p>
     `;
-    cartItemDiv.addEventListener('click', () => {
+
+    const h3 = cartItemDiv.querySelector('h3');
+    h3.addEventListener('click', () => {
       window.location.href = `/productDetails?productId=${item.product.id}`;
     });
+
+    const decrementBtn = cartItemDiv.querySelector('.decrement');
+    const incrementBtn = cartItemDiv.querySelector('.increment');
+    const itemQuantity = cartItemDiv.querySelector('.item-quantity');
+    const itemTotalPrice = cartItemDiv.querySelector('.item-total-price');
+
+    function updatePrice() {
+      itemTotalPrice.textContent = (item.product.price * item.quantity).toFixed(2);
+    }
+
+    decrementBtn.addEventListener('click', () => {
+      const newQuantity = Math.max(item.quantity - 1, 0);
+      cart.updateItemQuantity(item.product.id, newQuantity);
+      item.quantity = newQuantity;
+      itemQuantity.textContent = newQuantity;
+      updatePrice();
+      updateTotalCost();
+    });
+
+    incrementBtn.addEventListener('click', () => {
+      const newQuantity = Math.min(item.quantity + 1, 10);
+      cart.updateItemQuantity(item.product.id, newQuantity);
+      item.quantity = newQuantity;
+      itemQuantity.textContent = newQuantity;
+      updatePrice();
+      updateTotalCost();
+    });
+
     cartSummary.appendChild(cartItemDiv);
 
     // update coût total
     totalCost += item.product.price * item.quantity;
   });
 
-  // Créer un élément pour afficher le coût total
-  const totalCostElement = document.createElement('h4');
-  totalCostElement.innerHTML = `Coût total : ${totalCost.toFixed(2)}€`;
+  updateTotalCost();
   cartSummary.appendChild(totalCostElement);
 }
+
 
   
   async function isLoggedIn() {
