e9cd0b7d5b1bc709d0e3b1e1220552cf6fb505c7
Enzo <you@example.commekerkenzo@gmail.com>
2023-04-18 14:56:28 +0200

    possibilité ajout nouveau produit

--- public/views/js/addProduct.js
+++ public/views/js/addProduct.js
@@ -1,28 +1,28 @@
 document.getElementById('add-product-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    const name = document.getElementById('name').value;
-    const price = parseFloat(document.getElementById('price').value);
-    const description = document.getElementById('description').value;
-    const imageUrl = document.getElementById('image-url').value;
-  
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const price = document.getElementById('price').value;
+  const description = document.getElementById('description').value;
+  const image_url = document.getElementById('image_url').value;
+
+  try {
     const response = await fetch('/api/add-product', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name: name,
-        price: price,
-        description: description,
-        image_url: imageUrl,
-      }),
+      body: JSON.stringify({ name, price, description, image_url }),
     });
-  
-    if (response.status === 201) {
+
+    if (response.ok) {
       alert('Produit ajouté avec succès');
-      window.location.href = '/'; // Rediriger vers la liste des produits
+      document.getElementById('add-product-form').reset();
     } else {
-      alert('Erreur lors de l\'ajout du produit');
+      const errorMessage = await response.text();
+      alert(`Erreur lors de l'ajout du produit : ${errorMessage}`);
     }
-  });
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout du produit :', error);
+  }
+});
