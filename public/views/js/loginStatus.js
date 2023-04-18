0b188b1e1c9d7842b6e70d5e12fa4209eafafc40
Enzo <you@example.commekerkenzo@gmail.com>
2023-04-18 14:55:08 +0200

    Possibilité ajout nouveau produit (il faut faire le bouton cependant)

--- public/views/js/loginStatus.js
+++ public/views/js/loginStatus.js
@@ -1,30 +1,47 @@
 async function updateLoginStatus() {
-    const response = await fetch('/api/isLoggedIn');
-    const { isLoggedIn, user } = await response.json();
-    
-    const loginContainer = document.getElementById('login-container');
-    loginContainer.innerHTML = '';
+  const response = await fetch('/api/isLoggedIn');
+  const { isLoggedIn, user } = await response.json();
 
-    if (isLoggedIn) {
-      const logoutButton = document.createElement('button');
-      logoutButton.textContent = 'Déconnexion';
-      logoutButton.onclick = async () => {
-        await fetch('/api/logout');
-        updateLoginStatus();
-      };
-      loginContainer.appendChild(logoutButton);
+  const loginContainer = document.getElementById('login-container');
+  loginContainer.innerHTML = '';
 
-      const userInfo = document.createElement('span');
-      userInfo.textContent = `Connecté en tant que : ${user.email}`;
-      loginContainer.appendChild(userInfo);
-    } else {
-      const loginButton = document.createElement('button');
-      loginButton.innerHTML = '<img src="img/login.png" alt="Login">';
-      loginButton.onclick = () => {
-        window.location.href = '/login';
-      };
-      loginContainer.appendChild(loginButton);
-    }
+  if (isLoggedIn) {
+    const logoutButton = document.createElement('button');
+    logoutButton.textContent = 'Déconnexion';
+    logoutButton.onclick = async () => {
+      await fetch('/api/logout');
+      updateLoginStatus();
+    };
+    loginContainer.appendChild(logoutButton);
+
+    const userInfo = document.createElement('span');
+    userInfo.textContent = `Connecté en tant que : ${user.email}`;
+    loginContainer.appendChild(userInfo);
+
+    // Appel de la fonction pour afficher le bouton "Ajouter un produit"
+    showAddProductButton(user);
+
+  } else {
+    const loginButton = document.createElement('button');
+    loginButton.innerHTML = '<img src="img/login.png" alt="Login">';
+    loginButton.onclick = () => {
+      window.location.href = '/login';
+    };
+    loginContainer.appendChild(loginButton);
   }
+}
 
-  updateLoginStatus();
+function showAddProductButton(user) {
+  const addProductButton = document.getElementById('add-product-button');
+
+  if (user && user.role === 'admin') {
+    addProductButton.style.display = 'block';
+    addProductButton.onclick = () => {
+      window.location.href = '/ajouter-un-produit';
+    };
+  } else {
+    addProductButton.style.display = 'none';
+  }
+}
+
+updateLoginStatus();
