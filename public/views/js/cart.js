183c34910fb9f8a4894ff7ede3dc6cd8e16a7a18
Ismaiil23 <mkisma84@gmail.com>
2023-04-21 09:51:48 +0200

    fonctionnel css manquant

--- public/views/js/cart.js
+++ public/views/js/cart.js
@@ -1,48 +1,66 @@
 class Cart {
-    constructor() {
-      this.items = JSON.parse(localStorage.getItem('cart')) || [];
-    }
-  
-    addItem(product, quantity) {
-      const existingItem = this.items.find((item) => item.product.id === product.id);
-  
-      if (existingItem) {
-        existingItem.quantity += quantity;
-      } else {
-        this.items.push({ product, quantity });
-      }
-  
-      localStorage.setItem('cart', JSON.stringify(this.items));
-    }
-  
-    removeItem(productId) {
-      this.items = this.items.filter((item) => item.product.id !== productId);
-      localStorage.setItem('cart', JSON.stringify(this.items));
-    }
-  
-    updateItemQuantity(productId, quantity) {
-      const item = this.items.find((item) => item.product.id === productId);
-  
-      if (item) {
-        item.quantity = quantity;
-        localStorage.setItem('cart', JSON.stringify(this.items));
-      }
-    }
-  
-    getItems() {
-      return this.items;
-    }
-  
-    getTotal() {
-      return this.items.reduce((total, item) => total + item.product.price * item.quantity, 0);
-    }
-  
-    clear() {
-      this.items = [];
-      localStorage.removeItem('cart');
+  constructor() {
+    this.items = JSON.parse(localStorage.getItem('cart')) || [];
+    this._listeners = [];
+  }
+
+  _notify() {
+    this._listeners.forEach((listener) => listener());
+  }
+
+  on(event, callback) {
+    if (event === 'change') {
+      this._listeners.push(callback);
     }
   }
-  
-  const cart = new Cart();
-  
-  
+
+  addItem(product, quantity) {
+    const existingItem = this.items.find((item) => item.product.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      this.items.push({ product, quantity });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(this.items));
+    this._triggerEvent('change', { product, quantity });
+  }
+
+  removeItem(productId) {
+    this.items = this.items.filter((item) => item.product.id !== productId);
+    localStorage.setItem('cart', JSON.stringify(this.items));
+    this._triggerEvent('change');
+  }
+
+  updateItemQuantity(productId, quantity) {
+    const item = this.items.find((item) => item.product.id === productId);
+
+    if (item) {
+      item.quantity = quantity;
+      localStorage.setItem('cart', JSON.stringify(this.items));
+      this._triggerEvent('change');
+    }
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getTotal() {
+    return this.items.reduce((total, item) => total + item.product.price * item.quantity, 0);
+  }
+
+  clear() {
+    this.items = [];
+    localStorage.removeItem('cart');
+    this._triggerEvent('change');
+  }
+
+  _triggerEvent(eventName, data) {
+    const event = new CustomEvent(eventName, { detail: data });
+    document.dispatchEvent(event);
+  }
+}
+
+const cart = new Cart();
