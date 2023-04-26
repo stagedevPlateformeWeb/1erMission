2362fe0d45e3392b9e79a675049f65d2be9775b5
v21215959 <jules.volpei@etu.univ-amu.fr>
2023-04-26 16:58:12 +0200

    Carousel avec trois images aléatoires

--- public/views/js/fetchProducts.js
+++ public/views/js/fetchProducts.js
@@ -53,6 +53,50 @@ function updateCartCount() {
   cartCount.textContent = totalCount;
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex != 0) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 // fetchProducts sans paramètre pour charger tous les produits au début
 fetchProducts();
 updateCartCount();
+
+let listeImages = ["https://nospensees.fr/wp-content/uploads/2016/10/Femme-buvant-de-leau.jpg", "https://consommerpropre.fr/wp-content/uploads/2021/03/potomanie-besoin-irrepressible-de-boire-de-l-eau_1b3cf37d6f14c45a2bebc1c94757fdffe4660546.jpeg", "https://images.radio-canada.ca/v1/ici-premiere/16x9/eclr-verre-eau.jpg"];
+
+var indiceCourant = 1;
+
+listeImages = shuffle(listeImages);
+
+document.getElementById("image-instagram").src = listeImages[indiceCourant];
+
+const boutonGaucheCarousel = document.getElementById("bouton-gauche");
+
+boutonGaucheCarousel.addEventListener("click", () => {
+  if (indiceCourant == 1 || indiceCourant == 2) {
+    indiceCourant -= 1;
+  }
+  document.getElementById("image-instagram").src = listeImages[indiceCourant];
+});
+
+
+const boutonDroitCarousel = document.getElementById("bouton-droite");
+
+
+if (boutonDroitCarousel) {
+  boutonDroitCarousel.addEventListener("click", () => {
+    if (indiceCourant == 0 || indiceCourant == 1) {
+      indiceCourant += 1;
+    }
+    document.getElementById("image-instagram").src = listeImages[indiceCourant];
+  });
+}
