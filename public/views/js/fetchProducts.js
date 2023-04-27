1605cd7f9a8eef1e9ac71fa3d0d73475b0004906
v21215959 <jules.volpei@etu.univ-amu.fr>
2023-04-27 18:17:44 +0200

    Changement design carousel et modification du back pour le json

--- public/views/js/fetchProducts.js
+++ public/views/js/fetchProducts.js
@@ -67,36 +67,72 @@ function shuffle(array) {
   return array;
 }
 
+function carouselManager(apiInsta) {
+    const carousel = document.querySelector(".carousel-images-container");
+    for (let i = 0; i < 3; i += 1) {
+        let input = document.createElement("input");
+        input.type = "radio";
+        input.name = "carousel-image-slider";
+        input.id = "slider-" + (i + 1);
+        input.classList.add("carousel-image-slider");
+        if (i === 0) {
+            input.checked = true;
+        } else {
+            input.checked = false;
+        }
+        console.log(carousel);
+        console.log(input);
+        carousel.appendChild(input);
+    }
+    const carouselImages = document.querySelector(".carousel-images");
+
+    let jsonCarousel = shuffle(apiInsta.publications);
+
+    for (let i = 0; i < jsonCarousel.length; i++) {
+        let labelPublication = document.createElement("label");
+        labelPublication.setAttribute("for", "carousel-" + (i + 1));
+        labelPublication.classList.add("publication-" + (i + 1));
+        labelPublication.id = "publication-" + (i + 1);
+
+        let cartePublication = document.createElement("div");
+        cartePublication.classList.add("carousel-image");
+        let imagePublication = document.createElement("img");
+        imagePublication.src = jsonCarousel[i].image_url;
+        cartePublication.appendChild(imagePublication);
+        let divPoints = document.createElement("div");
+        divPoints.classList.add("points");
+
+        for (let j = 0; j < 4; j += 1) {
+            let point = document.createElement("div");
+            divPoints.appendChild(point);
+        }
+        cartePublication.appendChild(divPoints);
+
+        let informationPublication = document.createElement("div");
+        informationPublication.classList.add("info-publication");
+        let titrePublication = document.createElement("span");
+        titrePublication.classList.add("titre-publication");
+        titrePublication.textContent = jsonCarousel[i].titre;
+        informationPublication.appendChild(titrePublication);
+
+        labelPublication.appendChild(cartePublication);
+        labelPublication.appendChild(informationPublication);
+
+        carouselImages.appendChild(labelPublication);
+    }
+
+    carousel.appendChild(carouselImages);
+
+}
+
+
 // fetchProducts sans paramètre pour charger tous les produits au début
 fetchProducts();
 updateCartCount();
+var jsonString = '{"publications": ' +
+    '[{"titre": "Chocolat au lait", "image_url": "https://www.bonschocolatiers.com/_Images/Products/palomas/1280/0111000802-1-Tablette-Chocolat-Lait-45-90g-180515.jpg", "likes": 300}, ' +
+    '{"titre": "Chocolat noir", "image_url": "https://joel-vilcoq-chocolatier.fr/152-large_default/chocolats.jpg", "likes": 299}, ' +
+    '{"titre": "Chocolat blanc", "image_url": "https://chaudun.com/wp-content/uploads/2017/11/tablette-chocolat-blanc.png", "likes": 0}' +
+    ']}';
+carouselManager(JSON.parse(jsonString));
 
-let listeImages = ["https://nospensees.fr/wp-content/uploads/2016/10/Femme-buvant-de-leau.jpg", "https://consommerpropre.fr/wp-content/uploads/2021/03/potomanie-besoin-irrepressible-de-boire-de-l-eau_1b3cf37d6f14c45a2bebc1c94757fdffe4660546.jpeg", "https://images.radio-canada.ca/v1/ici-premiere/16x9/eclr-verre-eau.jpg"];
-
-var indiceCourant = 1;
-
-listeImages = shuffle(listeImages);
-
-document.getElementById("image-instagram").src = listeImages[indiceCourant];
-
-const boutonGaucheCarousel = document.getElementById("bouton-gauche");
-
-boutonGaucheCarousel.addEventListener("click", () => {
-  if (indiceCourant == 1 || indiceCourant == 2) {
-    indiceCourant -= 1;
-  }
-  document.getElementById("image-instagram").src = listeImages[indiceCourant];
-});
-
-
-const boutonDroitCarousel = document.getElementById("bouton-droite");
-
-
-if (boutonDroitCarousel) {
-  boutonDroitCarousel.addEventListener("click", () => {
-    if (indiceCourant == 0 || indiceCourant == 1) {
-      indiceCourant += 1;
-    }
-    document.getElementById("image-instagram").src = listeImages[indiceCourant];
-  });
-}
